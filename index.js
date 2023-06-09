ef7a58e8c8de4f3842d9d517f9ce22d91e832375
ofins <jkyeewang@gmail.com>
2023-06-09 13:13:16 +0800

    updated fetch URL to render.com and mongoDB database to cloud instead of Localhost

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect('mongodb://127.0.0.1:27017/myDB')
-// mongoose.connect('mongodb+srv://ofins:Password@nodeexpressproject.o28jccr.mongodb.net/?retryWrites=true&w=majority')
+// mongoose.connect('mongodb://127.0.0.1:27017/myDB')
+mongoose.connect('mongodb+srv://ofins:Password@nodeexpressproject.o28jccr.mongodb.net/?retryWrites=true&w=majority')
 
 const Team = require('./teams')
 const Game = require('./games')
